8a114435c7d2ed7caa0c2ab679a2a49100e54782
Justinas <justasrimavicius@gmail.com>
2022-05-17 21:26:40 +0300

    HTML    added additional tags for slider functionality/display JS      created a slider JS file - it shows the value of the slider below(also will be used for the grid later on), changes the background color of the slider by changing CSS linear radiant values CSS     created slider display

---
+++ script.js
@@ -0,0 +1,13 @@
+const gridContainer=document.getElementById('grid');
+//grid width,height is 400px
+
+// dimensions of the grid are inputed by a slider
+function DimensionsSlider(){
+    
+}
+
+
+// the dimensions are then returned to CreateGrid function
+function CreateGrid(dimensions){
+
+}
